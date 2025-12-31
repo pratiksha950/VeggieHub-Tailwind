50fd38023cace33d2552d438d0ee036097f11cb1
pratiksha950 <salunkepratiksha950@gmail.com>
2025-12-31 22:29:46 +0530

    basic homepage created:

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,18 +2,30 @@ import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
 import '../index.css'
+import imgMainHeader from '../assets/main-header.png'
 
 function Home() {
   return (
     <div>
       <Navbar />
 
-      <div className="min-h-screen bg-blue-300 flex justify-center">
-        <h1 className="pt-6 text-3xl font-bold">
-          Welcome to VeggieHub
+      <div className="min-h-screen bg-blue-300">
+        <div className='flex items-center'>
+        <img src={imgMainHeader} alt="Main Header" className="h-[500px]"/>
+        <div>
+        <h1 className="pt-0 text-6xl! font-normal! text-green-900">
+          From farm to your doorstep,
+          we deliver freshness you can trust.
         </h1>
-      </div>
+        </div>
+    </div>
 
+    <h1 className="pt-0 text-4xl! font-bold! text-black-900">
+      Taaza sabzi har din,
+      healthy zindagi ke liye.
+    </h1>
+
+      </div>
       <Footer />
     </div>
   )
--- src/views/Navbar.jsx
+++ src/views/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import {Salad} from 'lucide-react'
 
 function Navbar() {
   return (
-    <div className='sticky top-0 w-full bg-green-500 h-12 flex items-center px-4'>Navbar</div>
+    <div className='sticky top-0 w-full bg-green-500 h-20 flex items-center px-4'>
+      <Salad className='mr-2 ' size={40}/>
+      <h2 className='text-2xl font-bold'>VeggieHub</h2>
+    </div>
   )
 }
 
