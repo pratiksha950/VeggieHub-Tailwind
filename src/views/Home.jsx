bfa4ed9aafef7479dbeb113a89237bce619f033a
pratiksha950 <salunkepratiksha950@gmail.com>
2025-12-31 20:13:41 +0530

    imported tailwind library:

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,14 +3,17 @@ import Navbar from './Navbar'
 import Footer from './Footer'
 import '../index.css'
 
-
 function Home() {
   return (
     <div>
       <Navbar />
-      <div className='min-h-screen bg-blue-300'>
-        <h1>Welcome to VeggieHub</h1>
+
+      <div className="min-h-screen bg-blue-300 flex justify-center">
+        <h1 className="pt-6 text-3xl font-bold">
+          Welcome to VeggieHub
+        </h1>
       </div>
+
       <Footer />
     </div>
   )
--- src/views/Navbar.jsx
+++ src/views/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='sticky'>Navbar</div>
+    <div className='sticky top-0 w-full bg-green-500 h-12 flex items-center px-4'>Navbar</div>
   )
 }
 
