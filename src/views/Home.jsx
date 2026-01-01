01eac78feffa838772655b84de958391f4be0788
pratiksha950 <salunkepratiksha950@gmail.com>
2026-01-01 09:37:38 +0530

    feature card component created:

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,15 +3,37 @@ import Navbar from './Navbar'
 import Footer from './Footer'
 import '../index.css'
 import imgMainHeader from '../assets/main-header.png'
+import IconFresh from '../assets/fresh.png'
+import IconQuality from '../assets/high-quality.png'
+import IconSatisfaction from '../assets/satisfaction.png'
+import FeatureCard from '../components/FeatureCard'
+
+const FEATURES_CONFIG=[
+  {
+    title:"Freshness Guaranteed",
+    description:"We source our vegetables directly from trusted local farms to ensure the highest quality and freshness.",
+    img:IconFresh
+  },
+  {
+    title:"Quality Assurance",
+    description:"Our vegetables are carefully selected and inspected to meet the highest quality standards.",
+     img:IconQuality
+  },
+  {
+    title:"Customer Satisfaction",
+    description:"We are committed to providing excellent customer service and ensuring your satisfaction with every order.",
+     img:IconSatisfaction
+  },
+]
 
 function Home() {
   return (
     <div>
       <Navbar />
 
-      <div className="min-h-screen bg-blue-300">
+      <div className=" bg-green-200">
         <div className='flex items-center'>
-        <img src={imgMainHeader} alt="Main Header" className="h-[500px]"/>
+        <img src={imgMainHeader} alt="Main Header" className="h-[500px] w-auto object-contain"/>
         <div>
         <h1 className="pt-0 text-6xl! font-normal! text-green-900">
           From farm to your doorstep,
@@ -20,12 +42,27 @@ function Home() {
         </div>
     </div>
 
-    <h1 className="pt-0 text-4xl! font-bold! text-black-900">
+    <h1 className="pt-0 text-4xl! font-bold! text-black-900 pb-8 text-center">
       Taaza sabzi har din,
       healthy zindagi ke liye.
     </h1>
 
       </div>
+
+  <div className="flex justify-around bg-green-200 py-8 gap-9">
+    {FEATURES_CONFIG.map((feature) => {
+  const { title, description, img } = feature;
+
+  return (
+    <>
+      <FeatureCard title={title} description={description} img={img} />
+    </>
+  );
+})}
+
+    
+  
+    </div>      
       <Footer />
     </div>
   )
