68fca9e91ef4201b3f1e368ae6532afd17066e66
pratiksha950 <salunkepratiksha950@gmail.com>
2026-01-02 15:23:12 +0530

    footer added:

--- src/views/Footer.jsx
+++ src/views/Footer.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
+import { Instagram,Youtube,Linkedin,Github} from 'lucide-react'
+
+const socialLinks=[
+  {
+    name:"Github",
+    url:"https://github.com/pratiksha950?tab=repositories",
+    Icon:<Github />
+  },
+  {
+    name:"LinkedIn",
+    url:"linkedin.com/in/pratiksha950",
+    Icon:<Linkedin />
+  },
+  {
+    name:"Youtube",
+    url:"youtube.com/@pratiksha950",
+    Icon:<Youtube />
+  },
+  {
+    name:"Instagram",
+    url:"instagram.com/pratiksha950/",
+    Icon:<Instagram />
+  }
+]
+
+const FooterLink=({name,url,Icon})=>{
+  return(
+    <a href={url} className='inline mx-2 align-middle'>
+      {Icon} {name}
+    </a>
+  )
+}
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <>
+
+    <div className='w-full bg-green-500 h-16 flex items-center justify-center'>
+      Made with ❤️ by VeggieHub
+    </div>
+    <div className='w-full bg-green-500 h-12 flex items-center justify-center  '>
+            {socialLinks.map(({ name, url, Icon }, index) => {
+        return (
+          <FooterLink
+            key={index}
+            name={name}
+            url={url}
+            Icon={Icon}
+          />
+        );
+      })}
+
+    </div>
+
+
+    </>
   )
 }
 
